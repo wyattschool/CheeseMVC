a2b4caa0036a2a0a0c3f96a0915f7146c701d747
Wyatt School <wwebb@lakelandcollege.edu>
2022-04-27 23:08:54 -0500

    Add remove view, add remove functionality, and add some input checking to Add function.

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CheeseMVC.Controllers
+{
+	public class CheeseController : Controller
+	{
+		static private List<string> Cheeses = new List<string>();
+		public IActionResult Index()
+		{
+			ViewBag.cheeses = Cheeses;
+
+			return View();
+		}
+
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[Route("/Cheese/Add")]
+		public IActionResult NewCheese(string name)
+		{
+			if(name == null || name == "" || name == " ")
+			{
+				//Don't let the user enter nothing into the list
+				return View("Add");
+			}
+		
+			if (name == "1" || name == "2")
+			{
+				//Don't let the user enter a number into the list
+				return View("Add");
+			}
+			else
+			{
+				//Add what the user input to the form to the list then show them the list on the other page
+				Cheeses.Add(name);
+				return Redirect("/Cheese");
+			}
+		}
+
+		public IActionResult Remove()
+		{
+			ViewBag.cheeses = Cheeses;
+			return View();
+		}
+
+		[HttpPost]
+		[Route("/Cheese/Remove")]
+		public IActionResult RemoveCheese(string name)
+		{
+			Cheeses.Remove(name);
+			return Redirect("/Cheese");
+		}
+	}
+}
